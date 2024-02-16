cc3024dc59377aec8ecb6a4ef3ebe7a11bee1687
dbasu03 <basudeepayan123@gmail.com>
2024-02-16 07:15:52 -0800

    uploading on github(1)

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/grocery' element={<ShopCategory category="grocery"/>}/>
+        <Route path='/items' element={<ShopCategory category="items"/>}/>
+        <Route path='/fashion' element={<ShopCategory category="fashion"/>}/>
+        <Route path='/product' element={<Product/>}/>
+          <Route path=':productId' element={<Product/>}/>
+        <Route/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
